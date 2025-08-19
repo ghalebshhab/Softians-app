80586beafc27719644c14115e08fad1348e0323b
ghalebshhab <ghalebshhab12@icloud.com>
2025-08-19 19:02:48 +0300

    added the project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Test from './Test';
+
+function App() {
+  return (
+    <Test/>
+  );
+}
+
+export default App;
---
+++ src/Test.js
@@ -0,0 +1,265 @@
+import CircleIcon from "@mui/icons-material/Circle";
+import TuneIcon from "@mui/icons-material/Tune";
+import SplitText from "./Animation/SplitText";
+import LightRays from "./Animation/LightRays";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import ShinyText from "./Animation/ShinyText";
+import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import ClickSpark from "./Animation/ClickSpark";
+import ShapeBlur from "./Animation/ShapeBlur";
+import SpotlightCard from "./Animation/SpotlightCard";
+export default function Test() {
+  const handleAnimationComplete = () => {
+    console.log("All letters have animated!");
+  };
+  return (
+    <>
+      <div
+        id="Container"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          maxWidth: "100%",
+          height: "100vh",
+          overflow: "visible",
+        }}
+      >
+        <div style={{ width: "100%", height: "600px", position: "relative" }}>
+          <LightRays
+            raysOrigin="top-center"
+            raysColor="white"
+            raysSpeed={1.5}
+            lightSpread={0.8}
+            rayLength={1.2}
+            followMouse={true}
+            mouseInfluence={0.1}
+            noiseAmount={0.1}
+            distortion={0.05}
+            className="custom-rays"
+          />
+        </div>
+        <div
+          id="Header"
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            position: "fixed",
+            width: "100%",
+            zindex: "1000",
+          }}
+        >
+          <div
+            id="name"
+            style={{
+              width: "90%",
+              display: "flex",
+              alignSelf: "flex-start",
+              marginTop: "5px",
+              maxHeight: "20%",
+              height: "10vh",
+            }}
+          >
+            <CircleIcon
+              style={{
+                color: "white",
+                fontSize: "32px",
+                display: "flex",
+                alignSelf: "center",
+              }}
+            />
+            <h1
+              style={{ display: "flex", alignSelf: "center", color: "white" }}
+            >
+              <SplitText
+                style={{}}
+                text="Softians"
+                className="text-2xl font-semibold text-center"
+                delay={100}
+                duration={0.6}
+                ease="power3.out"
+                splitType="chars"
+                from={{ opacity: 0, y: 40 }}
+                to={{ opacity: 1, y: 0 }}
+                threshold={0.1}
+                rootMargin="-100px"
+                textAlign="center"
+                onLetterAnimationComplete={handleAnimationComplete}
+              />
+            </h1>
+          </div>
+          <div
+            id="threeicon"
+            style={{ width: "10%", display: "flex", alignSelf: "flex-end" }}
+          >
+            <button
+              style={{
+                marginBottom: "15px",
+                border: "none",
+                alignSelf: "center",
+                backgroundColor: "black",
+              }}
+            >
+              <HomeRoundedIcon
+                style={{
+                  color: "white",
+                  backgroundColor: "black",
+                  fontSize: "32px",
+                  marginLeft: "50px",
+                }}
+              />
+            </button>
+          </div>
+        </div>
+        <div
+          id="content"
+          style={{
+            position: "absolute",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            alignContent: "center",
+            width: "100%",
+            marginBottom: "100px",
+          }}
+        >
+          <h1
+            style={{
+              display: "flex",
+              alignSelf: "center",
+              color: "white",
+              marginTop: "50px",
+            }}
+          >
+            <ShinyText
+              text="Welcome To Softians Website"
+              disabled={false}
+              speed={3}
+              className="custom-class"
+            />
+          </h1>
+          <div
+            id="Majors"
+            style={{
+              color: "white",
+              display: "flex",
+              flexDirection: "column",
+              alignSelf: "center",
+            }}
+          >
+            <h1 style={{ alignSelf: "center", display: "flex" }}>
+              <ClickSpark
+                sparkColor="#fff"
+                sparkSize={20}
+                sparkRadius={30}
+                sparkCount={8}
+                duration={400}
+              >
+                Majors
+              </ClickSpark>
+            </h1>
+            <div
+              style={{
+                alignSelf: "center",
+                display: "flex",
+                alignContent: "center",
+                flexDirection: "column",
+              }}
+            >
+              <h1 style={{ textAlign: "center" }}>
+                {" "}
+                <SpotlightCard
+                  style={{ alignSelf: "center", width: "30%" }}
+                  className="custom-spotlight-card"
+                  spotlightColor="rgba(0, 229, 255, 0.2)"
+                >
+                  Software
+                </SpotlightCard>
+              </h1>
+              <h1 style={{ textAlign: "center" }}>
+                {" "}
+                <SpotlightCard
+                  style={{ alignSelf: "center", width: "30%" }}
+                  className="custom-spotlight-card"
+                  spotlightColor="rgba(0, 229, 255, 0.2)"
+                >
+                  Computer Sicence
+                </SpotlightCard>
+              </h1>
+              <h1 style={{ textAlign: "center" }}>
+                {" "}
+                <SpotlightCard
+                  style={{ alignSelf: "center", width: "30%" }}
+                  className="custom-spotlight-card"
+                  spotlightColor="rgba(0, 229, 255, 0.2)"
+                >
+                  Artificial Intellegence
+                </SpotlightCard>
+              </h1>
+              <h1 style={{ textAlign: "center" }}>
+                {" "}
+                <SpotlightCard
+                  style={{ alignSelf: "center", width: "30%" }}
+                  className="custom-spotlight-card"
+                  spotlightColor="rgba(0, 229, 255, 0.2)"
+                >
+                  Cyper Security
+                </SpotlightCard>
+              </h1>
+            </div>
+          </div>
+        </div>
+        <div
+          id="Bottom"
+          style={{
+            alignSelf: "flex-end",
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            marginTop: "200px",
+            zIndex: "1000",
+            justifyContent: "center",
+            position: "fixed",
+            bottom: "0px",
+          }}
+        >
+          <div
+            id="Links"
+            style={{
+              display: "flex",
+              alignSelf: "flex-end",
+              flexDirection: "row",
+              justifyContent: "center", // ✅ centers horizontally
+              alignItems: "center", // optional spacing from bottom
+            }}
+          >
+            <button
+              style={{
+                border: "none",
+                backgroundColor: "transparent",
+                display: "flex",
+              }}
+            >
+              <FacebookRoundedIcon
+                style={{ color: "white", fontSize: "32px" }}
+              />
+            </button>
+            <button style={{ border: "none", backgroundColor: "transparent" }}>
+              <YouTubeIcon style={{ color: "white", fontSize: "32px" }} />
+            </button>
+            <button
+              style={{
+                border: "none",
+                backgroundColor: "transparent",
+                display: "flex",
+              }}
+            >
+              <LinkedInIcon style={{ color: "white", fontSize: "32px" }} />
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
